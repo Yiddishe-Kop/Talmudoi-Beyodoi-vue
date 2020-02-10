8356b80481459d00cc5979b717cd8c9df478fa01
Yehuda Neufeld <newgraphil@gmail.com>
2020-02-10 23:26:08 +0200

    added commentaries

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,47 +2,66 @@
   <div class="home relative flex flex-col" @click="translation = ''">
     <control-panel @get-text="getText" />
 
-    <main class="pt-8 overflow-scroll">
+    <section style="height: calc(100vh - 130px);">
       <loader v-if="state === 1" />
 
-      <div v-else-if="state === 2" class="text pb-2 text-2xl text-justify leading-relaxed">
-        <div class="title-bar flex items-center justify-between">
-          <h2 class="ml-2 font-bold text-gray-600">{{json.heRef}}</h2>
-          <div class="actions flex">
-            <ui-button
-              v-if="json.prev"
-              @click.native="getText(json.prev)"
-              classes="text-sm py-1 ml-1"
-            >הקודם</ui-button>
-            <ui-button
-              v-if="json.next"
-              @click.native="getText(json.next)"
-              classes="text-sm py-1"
-            >הבא</ui-button>
+      <div
+        v-else-if="state === 2"
+        class="h-full flex text pb-2 text-2xl text-justify leading-relaxed"
+      >
+        <div class="relative main-content pt-8 w-3/4 h-full px-6 overflow-scroll">
+          <div class="title-bar flex items-center justify-between">
+            <h2 class="ml-2 font-bold text-gray-600">{{json.heRef}}</h2>
+            <div class="actions flex">
+              <ui-button
+                v-if="json.prev"
+                @click.native="getText(json.prev)"
+                classes="text-sm py-1 ml-1"
+              >הקודם</ui-button>
+              <ui-button
+                v-if="json.next"
+                @click.native="getText(json.next)"
+                classes="text-sm py-1"
+              >הבא</ui-button>
+            </div>
+          </div>
+          <span
+            v-for="(segment, i) in text"
+            class="segment"
+            :key="segment.length + Math.random()"
+            v-html="segment"
+            @click.stop="handleSegmentClick(i)"
+          ></span>
+
+          <transition name="slideUp">
+            <div
+              v-show="translation"
+              style="max-width: 80%; max-height: 120px; overflow: auto; margin: auto;"
+              class="translation absolute bottom-0 right-0 left-0 mb-8 text-base text-center bg-gray-800 text-gray-100 py-2 px-6 rounded"
+            >
+              <span v-html="translation"></span>
+            </div>
+          </transition>
+        </div>
+
+        <div class="commentaries w-1/4 h-full px-6 pt-8 overflow-scroll">
+          <loader v-if="commentary.state === 1" />
+
+          <div
+            v-else-if="commentary.state === 2"
+            v-for="com in commentaries"
+            :key="com._id"
+            class="mb-6 border-b border-gray-500"
+          >
+            <h2 class="font-bold">{{com.heTitle}}</h2>
+            <p class="text-sm" v-html="com.he.length ? com.he : com.text"></p>
           </div>
         </div>
-        <span
-          v-for="(segment, i) in text"
-          class="segment"
-          :key="segment.length + Math.random()"
-          v-html="segment"
-          @click.stop="translation = json.text[i]"
-        ></span>
       </div>
 
       <p v-else-if="state === 3">חיפוש זה לא העלה שום תוצאות 😕</p>
       <p v-else>חפש משהו למעלה 👆</p>
-    </main>
-
-    <transition name="slideUp">
-      <div
-        v-show="translation"
-        style="max-width: 80%; max-height: 120px; overflow: auto; margin: auto;"
-        class="translation absolute bottom-0 right-0 left-0 mb-8 text-center bg-gray-800 text-gray-100 py-2 px-6 rounded"
-      >
-        <span v-html="translation"></span>
-      </div>
-    </transition>
+    </section>
   </div>
 </template>
 
@@ -70,7 +89,11 @@ export default {
       translation: "",
       categories: [],
       state: STATE.EMPTY,
-      json: {}
+      json: {},
+      commentary: {
+        state: STATE.EMPTY,
+        commentaries: []
+      }
     };
   },
   methods: {
@@ -80,7 +103,7 @@ export default {
       this.translation = "";
       fetch(`${SEFARIA_API_URL}texts/${searchQuery}`).then(data => {
         data.json().then(json => {
-          console.log(json);
+          // console.log(json);
           if (json.error) {
             this.state = STATE.ERROR;
           } else {
@@ -92,15 +115,29 @@ export default {
           }
         });
       });
+    },
+    getLinks(ref) {
+      if (!ref) return;
+      this.commentary.state = STATE.LOADING;
+      fetch(`${SEFARIA_API_URL}links/${ref}`).then(data => {
+        data.json().then(json => {
+          console.log(json);
+          if (!json.error) {
+            this.commentary.state = STATE.READY;
+            this.commentaries = json;
+          }
+        });
+      });
+    },
+    handleSegmentClick(i) {
+      this.translation = this.json.text[i];
+      this.getLinks(`${this.json.ref}:${i + 1}`);
     }
   }
 };
 </script>
 
 <style lang="scss">
-main.overflow-scroll {
-  height: calc(100vh - 130px);
-}
 .text {
   span.segment {
     @apply px-1 cursor-pointer rounded;
